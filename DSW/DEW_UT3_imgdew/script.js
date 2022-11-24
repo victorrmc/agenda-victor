94e0ff938b982e30c265bde34c4ae28a60e1f987
Victor <victortenemc@gmail.com>
2022-11-24 11:44:44 +0000

    sol

--- DSW/DEW_UT3_imgdew/script.js
+++ DSW/DEW_UT3_imgdew/script.js
@@ -1,5 +1,4 @@
 let agenda = ["https://calzetonia.com/wp-content/uploads/2020/04/ea72ee53-1.jpg", "https://pandasneakers.es/wp-content/uploads/2021/06/nike-air-jordan-x-dior-hombre-1575528357-1.jpg"];
-
 // Función IIFE
 (function () {
     let storageImagen = JSON.parse(window.localStorage.getItem("imgdew"));
@@ -17,44 +16,56 @@ let agenda = ["https://calzetonia.com/wp-content/uploads/2020/04/ea72ee53-1.jpg"
         newimg = document.createElement("img")
         newimg.setAttribute("src", i)
         newdiv.insertAdjacentElement("beforeend", newimg);
+        newdiv.addEventListener("animationend", Eliminarhtml);
 
         newlist.insertAdjacentElement("afterbegin", newdiv);
-        newlist.addEventListener("click", seleccionar)
+        newlist.addEventListener("click", seleccionar);
     });
     // Lista de imágenes que se mostrarán de forma dinámica ...
 })()
 
 function Añadir(url) {
     let h1 = document.getElementsByTagName("h1")[0];
-    if(url == ""){
-        h1.textContent="Mete un url"
-        h1.style.color=red;
-    }else{
-        h1.textContent="Victor"
+    if (url == "") {
+        h1.textContent = "Mete un url"
+        h1.style.color = red;
+    } else {
+        h1.textContent = "Victor"
         agenda.push(url);
         newdiv = document.createElement("div")
-        newdiv.setAttribute("class", "imagen")
-        
-    
+        newdiv.setAttribute("class", "imagen");
+        newdiv.addEventListener("animationend", Eliminarhtml);
+
         newimg = document.createElement("img")
         newimg.setAttribute("src", agenda[agenda.length - 1])
         newdiv.insertAdjacentElement("afterbegin", newimg);
         document.getElementsByClassName("imagen")[0].insertAdjacentElement("beforebegin", newdiv)
         storageImagen = localStorage.setItem("imgdew", JSON.stringify(agenda));
+        
     }
-    
 }
-function Eliminar(){
-   eliminar = document.querySelectorAll(".imageneliminar");
-   document.getElementById("lista-imagenes").removeChild(eliminar);
+
+function Eliminar() {
+    let eliminar = document.querySelectorAll(".imageneliminar");
+    eliminar.forEach(function(e) {
+        e.classList.add("imageneliminada"); 
+    });
 }
-function seleccionar(e){
-    if(e.ctrlKey){
-        if(e.target.parentNode.classList.contains("imageneliminar")){
-        e.target.parentNode.classList.remove("imageneliminar");
-    }else{
-        e.target.parentNode.classList.add("imageneliminar");
+
+function Eliminarhtml(){
+   let  eliminar = document.querySelectorAll(".imageneliminar");
+    eliminar.forEach(function (e) {
+       src = e.firstElementChild.getAttribute("src");
+       indice = agenda.indexOf(src);
+       agenda.splice(indice, 1);
+        document.getElementById("lista-imagenes").removeChild(e);
+    });
+    storageImagen = localStorage.setItem("imgdew", JSON.stringify(agenda));
+    agenda = JSON.parse(localStorage.getItem("imgdew", JSON.stringify(agenda)));
+}
+
+function seleccionar(e) {
+    if (e.ctrlKey) {
+        e.target.parentNode.classList.toggle("imageneliminar");  
     }
-    }
-    
 }
